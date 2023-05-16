1899659a1d8bf98fb7583636be352f36cc710a1e
Juliana <aragaopintojuli@gmail.com>
2023-05-16 18:04:28 -0300

    bg image

--- src/pages/Log/LogOutlet.jsx
+++ src/pages/Log/LogOutlet.jsx
@@ -2,7 +2,10 @@ import { Outlet } from 'react-router-dom';
 
 function HomeOutlet() {
   return (
-    <main className="bg-[url('./assets/images/doggo.jpg')] bg-no-repeat bg-cover bg-center bg-fixed min-h-screen">
+    <main
+      className="bg-no-repeat bg-cover bg-center bg-fixed min-h-screen"
+      style={{ backgroundImage: '/assets/images/doggo.jpg' }}
+    >
       <div className="flex flex-col min-h-screen bg-gray-800 bg-opacity-50 p-2">
         <Outlet />
       </div>
